e53ef22a2a48ab2d77e0966c4b174f866761741f
Hunter Carey <huntercarey902@gmail.com>
2019-11-11 14:06:04 -0600

    added in two other API calls and the default state

---
+++ src/RecipeSearcher/RecipeSearcher.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class RecipeSearcher extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            recipes:[]
+        }
+    }
+
+    getRandomRecipe = () => {
+        axios({
+            /* We can configure everything we need to about the HTTP request in here */
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/random.php'
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+
+    getRecipeByName = name => {
+        axios({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                s: name
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(response) {
+            console.log(response);
+        })
+    }
+
+    getRecipesByLetter = letter => {
+        axios({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/search.php',
+            params: {
+                f: letter
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+
+    render() {
+
+        this.getRandomRecipe();
+        this.getRecipeByName('Chicken');
+        this.getRecipesByLetter('L');
+
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default RecipeSearcher;
